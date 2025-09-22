3cc16a01fc0295d4db5c5734cafd252106474ba6
Lexi <arallen3660@gmail.com>
2025-09-22 11:38:57 -0400

    Fix file uploading

--- scripts/calls.js
+++ scripts/calls.js
@@ -1,10 +1,23 @@
-function uploadMap(ev) {
-  const file = ev.target.files[0];
-  const parts = file.name.split();
-  parts.pop();
-
-  fetch(`/api/files/images/${parts.join('.')}?userid=gamemaster`, {
-    method: "POST",
-    body: file,
-  });
+async function mapFormSubmit(e) {
+    e.preventDefault();
+    const fileInput = document.getElementById('map-input');
+    const formData = new FormData();
+    if (fileInput.files.length > 0) {
+        formData.append('file', fileInput.files[0]);
+    } else {
+        return;
+    }
+    const file = fileInput.files[0];
+    const parts = file.name.split('.');
+    parts.pop();
+    try {
+        const response = await fetch(`/api/files/images/${parts.join('.')}?userid=gamemaster`, {
+            method: 'POST',
+            body: formData
+        });
+        const result = await response.json();
+        console.log("Upload successful:", result);
+    } catch (err) {
+        console.log("Upload failed: ", err);
+    }
 }
